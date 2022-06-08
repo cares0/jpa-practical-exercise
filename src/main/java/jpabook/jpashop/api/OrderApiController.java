9dc46db1e26f43871be75e77c72ce503bc7ac3d7
cares0 <cares00000@gmail.com>
2022-06-09 07:59:36 +0900

    컬렉션 주문 조회 V1 - 엔티티를 직접 노출

---
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -0,0 +1,36 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByCriteria(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName();
+            order.getDelivery().getAddress();
+            List<OrderItem> orderItems = order.getOrderItems();
+
+            // OrderItem과 Item 지연로딩 초기화
+            orderItems.stream().forEach(o -> o.getItem().getName());
+            // 위와 동일한 코드임
+            /*for (OrderItem orderItem : orderItems) {
+                orderItem.getItem().getName();
+            }*/
+        }
+        return all;
+    }
+}
